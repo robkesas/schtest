9893d43415a13906ef51a782785e336584e3b4f3
Robertas <robertas@robertas.com>
2019-04-01 23:02:01 +0300

    init commit

---
+++ src/main/java/degaluapskaita/Cekis.java
@@ -0,0 +1,13 @@
+package degaluapskaita;
+
+public class Cekis {
+
+    String pildymodata;
+    String degalurusis;
+    int oktanas;
+    double litrokaina;
+    double degalukiekis;
+    int odometroparodymai;
+
+
+}
